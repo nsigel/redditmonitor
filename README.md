a47a205f076161e2f5472d9485a1361bc3c62fa9
wadu! <noah.sigel@gmail.com>
2020-12-09 21:01:46 -0500

    Added support to change channel for specific subreddits
    And some more bugfxes/changes

--- README.md
+++ README.md
@@ -3,22 +3,25 @@ Script to monitor a subreddit and post to discord!
 
 ## Installation
 
-Download Python from https://www.python.org/downloads/
-Create a bot account at https://discord.com/developers/applications/
-Clone the repository!
-```
-git clone https://github.com/fourwadu/redditmonito.git
-```
-Install requirements
-```
-pip/pip3 install -r requirements.txt
-```
-Fill in the template in env.example with your bot token and Discord webhook links!
-Rename env.example to .env
-Start it!
-```
-python/python3 main.py
-```
+1. Download Python from https://www.python.org/downloads/  
+2. Create a bot account at https://discord.com/developers/applications/  
+3. Clone the repository!
+    ```
+    git clone https://github.com/fourwadu/redditmonitor.git
+    ```
+4. Install requirements  
+    ```
+    pip/pip3 install -r requirements.txt
+    ```
+5. Fill in the template in env.example with your bot token default Discord channel!  
+
+8. Rename env.example to .env
+
+7. Start the bot!
+    ```
+    python/python3 main.py
+    ```
+
 ## Contributing
 Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are greatly appreciated.
 
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import aiohttp # this is what we're going to use to make http requests to reddit
 import json # this is how we will manupulate the json response of the reddit API
 import os
 from dotenv import load_dotenv
-from discord_webhook import  DiscordEmbed, DiscordWebhook
+from datetime import datetime
 
 load_dotenv() #load environment variables from the .env file
 TOKEN = os.getenv("BOT_TOKEN")
@@ -14,32 +14,36 @@ client = commands.Bot(command_prefix="r!") # this defines the discord bot's obje
 @client.event 
 async def on_ready(): # this bot event is called when the discord API returns a "ready" status.
     print(f"Alive and well as {client.user}!")  
-
     # Sets the json object content of "subreddits.json" as a client variable - accessable and mutable everywhere
     client.subreddits = json.load(open("subreddits.json"))
 
     # Changes the bot's discord precense status!
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"over {len(client.subreddits)} subreddits!"))
-
     await start_monitor()
-
-
 
 async def start_monitor():
     client.connector = aiohttp.TCPConnector(ssl=False, limit=None)
-    client.session = await aiohttp.ClientSession(connector=client.connector).__aenter__()
+    client.session = await aiohttp.ClientSession(connector=client.connector).__aenter__() # this creates a session within aiohttp and converts it into an awaitable using "__aenter__" so we can use it throughout the script!
     while True:
         await asyncio.wait([monitor(subreddit) for subreddit in [*client.subreddits]]) # calls the monitor function for each subreddit in the subreddits
 
 @client.command()
 async def add_subreddit(ctx, subreddit):
     client.subreddits[subreddit.replace('r/', '').replace('/', '')] = {
-        "last_post": 0
+        "last_post": 0,
+        "channel": int(os.getenv("DEFAULT_CHANNEL"))
     }
 
     json.dump(client.subreddits, open("subreddits.json", "w"), indent=4)
-    await ctx.send(embed=discord.Embed(title=f"Succesfully added subreddit {subreddit}!", color=0x0066CC))
+    await ctx.send(embed=discord.Embed(title=f"Succesfully added subreddit {subreddit}!", description=f"Change this subreddit's posting channel with `r!channel {subreddit}`", color=0x0066CC))
 
+
+@client.command()
+async def channel(ctx, subreddit, channel):
+    if subreddit in [*client.subreddits]:
+        client.subreddits[subreddit]["channel"] = channel
+    await ctx.send(embed=discord.Embed(title=f"Succesfully changed channel!", description=f"Channel changed to `{channel}`", color=0x0066CC))
+        
 
 @client.command()
 async def remove_subreddit(ctx, subreddit):
@@ -50,12 +54,10 @@ async def remove_subreddit(ctx, subreddit):
 
 @client.command()
 async def subreddits(ctx):
-    subs = []
-
-    for i in client.subreddits.keys():
-        subs.append(i)
-    
-    embed = discord.Embed(title="Monitored products:", description= "\n".join(subs), color=0x0066CC)
+    embed = discord.Embed(title="Monitored subreddits:", color=0x0066CC)
+    subs = [*client.subreddits]
+    for i in subs:
+        embed.add_field(name=i, value=client.subreddits[i]["channel"])
     await ctx.send(embed=embed)
 
 async def request(subreddit):
@@ -65,7 +67,6 @@ async def request(subreddit):
     }
 
     url = f"https://www.reddit.com/r/{subreddit}/new.json"
-
     return await client.session.get(url, headers=headers)
 
 
@@ -101,28 +102,50 @@ async def send_new_post(post):
     post_author = post["author"]
     selftext = post["selftext"]
     subreddit = post["subreddit"]
-    hooks = os.getenv("WEBHOOK_LINK").split(' ')
 
-    embed = DiscordEmbed(title=title , url=post_link, color=0x0066CC)
+    embed = discord.Embed(title=title , url=post_link, color=0x0066CC)
     
     if selftext:
-        embed.add_embed_field(name="Post selftext:", value=selftext)
+        embed.add_field(name="Post selftext:", value=selftext)
 
     if "i.redd.it" in link:
         embed.set_thumbnail(url=link)    
     else:
-        embed.add_embed_field(name="Linked URL:", value=post_link)
+        embed.add_field(name="Linked URL:", value=post_link)
 
     embed.set_author(icon_url="https://images-ext-2.discordapp.net/external/ZJezPe-pD7CzDpku0o5R2zTdEcksXkU3cCAulnhGjwQ/https/www.iconfinder.com/data/icons/social-media-2092/100/social-36-512.png", name=f"New post found in r/{subreddit}!")
-    embed.add_embed_field(name="Author:", value=post_author, inline=True)
-    embed.set_timestamp()
+    embed.add_field(name="Author:", value=post_author, inline=True)
+    embed.timestamp = datetime.now()
 
-    for i in hooks:
-        hook = DiscordWebhook(url=i)
-        hook.add_embed(embed)
-        hook.execute()
+    posting_channel = client.get_channel(client.subreddits[subreddit]["channel"])
+
+    await posting_channel.send(embed=embed)
 
     client.subreddits[subreddit]["last_post"] = posted
     json.dump(client.subreddits, open("subreddits.json", "w"), indent=4)
+
+# Error handling 
+
+@client.event
+async def on_command_error(ctx, error):
+    if hasattr(ctx.command, 'on_error'):
+        return
+
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send(embed=discord.Embed(color=0xE1306C, description="Not allowed. If you believe this is a mistake, please contact a developer!"))
+
+    elif isinstance(error, commands.CommandNotFound):
+        return
+
+    elif isinstance(error, commands.MissingRequiredArgument):
+        try:
+            await ctx.send(content=ctx.author.mention, embed=discord.Embed(color=0xE1306C, description=f"**Bad syntax!** Use {prefix}`help <command>` to get syntax."))
+        
+        except discord.Forbidden:
+            pass
+
+    else:
+        print(error)
+
 
 client.run(TOKEN)
